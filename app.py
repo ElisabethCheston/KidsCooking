8965c1bf51c1f5af507080de6a977de98d298a46
ElisabethCheston <elisabeth.cheston@gmail.com>
2021-04-01 14:21:28 +0000

    Install Heroku.

--- app.py
+++ app.py
@@ -4,5 +4,15 @@ if os.path.exists("env.py"):
     import env
 
 
+app = Flask(__name__)
 
-    
+
+@app.route("/")
+def hello():
+    return "Hello"
+
+
+    if __name__ == "__main__":
+        app.run(host=os.environ.get("IP"),
+        port=int(os.environ.get("PORT")),
+        debug=True)
