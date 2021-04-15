892e5099466da401a489a50edcea12665fd19f53
ElisabethCheston <elisabeth.cheston@gmail.com>
2021-04-15 15:11:29 +0000

    Create page for add_recipes and link it to base.html.

--- app.py
+++ app.py
@@ -97,6 +97,11 @@ def logout():
     session.pop("user")
     return redirect(url_for("login"))
 
+  # ---- Add recipes ----
+@app.route("/add_recipes")
+def add_recipes():
+    return render_template("add_recipes.html")
+
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
