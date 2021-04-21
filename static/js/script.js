99a6fa81f1436b52c146c82636ed58cbf89e9a30
ElisabethCheston <elisabeth.cheston@gmail.com>
2021-04-21 11:44:29 +0000

    Create table with recipe info under recipe images.

--- static/js/script.js
+++ static/js/script.js
@@ -1,30 +1,31 @@
 /*----- Materialize CSS JQuery Helpers -----*/
-$(document).ready(function(){
-    $(".sidenav").sidenav({edge: "right"});
-    $(".tooltipped").tooltip(); 
-    $("select").formSelect();    
+$(document).ready(function () {
+    $(".sidenav").sidenav({ edge: "right" });
+    $(".tooltipped").tooltip();
+    $("select").formSelect();
+    $('select').material_select();
     $(".parallax").parallax();
     $('.dropdown-trigger').dropdown();
     $(".fixed-action-btn").floatingActionButton();
     $(".modal").modal();
 });
 
+
 /*----- Display url image ----- 
 ----------- on AddRecipe Pages----------
 Adapted from https://stackoverflow.com/questions/31398473/
 load-image-in-div-from-url-obtained-from-a-text-box/31398762*/
-function addImage()
-{
-  var url = document.getElementById("url_img").value;
-  var image = new Image();
-  image.src = url;
-  document.getElementById("image-container").appendChild(image);
+function addImage() {
+    var url = document.getElementById("url_img").value;
+    var image = new Image();
+    image.src = url;
+    document.getElementById("image-container").appendChild(image);
 }
 
 /*---------- FAB for Recipe Edit & Delete ----------*/
-document.addEventListener("DOMContentLoaded", function() {
-var elems = document.querySelectorAll(".fixed-action-btn");
-var instances = M.FloatingActionButton.init(elems, {
-    direction: "left"
-});
+document.addEventListener("DOMContentLoaded", function () {
+    var elems = document.querySelectorAll(".fixed-action-btn");
+    var instances = M.FloatingActionButton.init(elems, {
+        direction: "left"
+    });
 });
