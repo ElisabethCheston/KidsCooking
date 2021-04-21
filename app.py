1a948af923c4934136423fbae55c31f2c5dc0b58
ElisabethCheston <elisabeth.cheston@gmail.com>
2021-04-21 09:05:45 +0000

    Create a 'all recipes link in navbar.

--- app.py
+++ app.py
@@ -204,7 +204,11 @@ def sweets():
 
 @app.route("/party")
 def party():
-    return render_template("party.html")                   
+    return render_template("party.html") 
+
+@app.route("/recipes")
+def recipes():
+    return render_template("recipes.html")                      
 
 
 if __name__ == "__main__":
