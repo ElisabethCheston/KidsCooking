f56370c232a8b556deebf88812ab6aab2c9710a9
ElisabethCheston <elisabeth.cheston@gmail.com>
2021-04-27 08:59:00 +0000

    Remove background on home page.

--- static/js/script.js
+++ static/js/script.js
@@ -20,3 +20,16 @@ function addImage() {
     image.src = url;
     document.getElementById("image-container").appendChild(image);
 }
+
+// - START POPUP MODAL ONLY SHOWN ONCE  - //
+
+$(window).on('load', function () {
+    $('#startModal').modal('show');
+});
+
+// - NAME LINK TO START MODE WITHOUT START MODAL - //
+
+$("#startView").click(function() {
+  infowindow.close();
+  map.fitBounds(bounds);
+});
