a81ee668757e08dc85134b9a68a5c5bd89a82cd3
ElisabethCheston <elisabeth.cheston@gmail.com>
2021-04-30 06:31:28 +0000

    Edit category buttons.

--- static/js/script.js
+++ static/js/script.js
@@ -4,17 +4,19 @@ $(document).ready(function () {
     $(".tooltipped").tooltip();
     $("select").formSelect();
     $(".parallax").parallax();
-    $(".modal").modal();
+    // $(".modal").modal();
 });
 
 /* --- Start Info Modal --- */
+
 $(document).ready(function () {
-    $('.modal').modal();
-    $('#modal1').modal('open');
+    $(".modal").modal();
+    $("#modal1").modal("open");
 });
 
 /* --- For Add Recipe Image --- */
 // Reference  https://stackoverflow.com/questions/31398473/load-image-in-div-from-url-obtained-from-a-text-box/31398762
+
 function addImage() {
     var url = document.getElementById("url_img").value;
     var image = new Image();
